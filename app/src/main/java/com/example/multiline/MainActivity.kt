cf77fc0c8aea3bc76fcd67c1d7c297dcc4185871
marcinkolacz1976@gmail.com <marcinkolacz1976@gmail.com>
2021-11-17 07:49:45 +0100

    Initial commit

---
+++ app/src/main/java/com/example/multiline/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.multiline
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        findViewById<Button>(R.id.Zmien).setOnClickListener {
+            findViewById<EditText>(R.id.multiText).setText("Marcin \n Kołacz")
+        }
+    }
+
+
+
+}
